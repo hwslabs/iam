087e8033206ab4733c6828f141396ce89063c311
Hari Narayanan <hari@hypto.in>
2022-02-05 01:24:07 +0530

    Initial commit fixing code gen errors.

---
+++ src/main/kotlin/com/hypto/iam/AppMain.kt
@@ -0,0 +1,90 @@
+package com.hypto.iam
+
+import com.codahale.metrics.Slf4jReporter
+import com.typesafe.config.ConfigFactory
+import io.ktor.application.Application
+import io.ktor.application.ApplicationStopping
+import io.ktor.application.install
+import io.ktor.application.log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.config.HoconApplicationConfig
+import io.ktor.features.AutoHeadResponse
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.HSTS
+import io.ktor.gson.GsonConverter
+import io.ktor.http.ContentType
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Locations
+import io.ktor.routing.Routing
+import java.util.concurrent.TimeUnit
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.auth.Authentication
+import io.ktor.auth.oauth
+import io.ktor.metrics.dropwizard.DropwizardMetrics
+import com.hypto.iam.infrastructure.ApiKeyCredential
+import com.hypto.iam.infrastructure.ApiPrincipal
+import com.hypto.iam.infrastructure.apiKeyAuth
+import com.hypto.iam.api.ActionApi
+import com.hypto.iam.api.CredentialApi
+import com.hypto.iam.api.OrganizationApi
+import com.hypto.iam.api.PolicyApi
+import com.hypto.iam.api.ResourceTypeApi
+import com.hypto.iam.api.TokenApi
+import com.hypto.iam.api.UsersApi
+
+@KtorExperimentalAPI
+internal val settings = HoconApplicationConfig(ConfigFactory.defaultApplication(HTTP::class.java.classLoader))
+
+object HTTP {
+    val client = HttpClient(Apache)
+}
+
+@KtorExperimentalAPI
+@KtorExperimentalLocationsAPI
+fun Application.main() {
+    install(DefaultHeaders)
+    install(DropwizardMetrics) {
+        val reporter = Slf4jReporter.forRegistry(registry)
+                .outputTo(log)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build()
+        reporter.start(10, TimeUnit.SECONDS)
+    }
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, GsonConverter())
+    }
+    install(AutoHeadResponse) // see http://ktor.io/features/autoheadresponse.html
+    install(HSTS, ApplicationHstsConfiguration()) // see http://ktor.io/features/hsts.html
+    install(Compression, ApplicationCompressionConfiguration()) // see http://ktor.io/features/compression.html
+    install(Locations) // see http://ktor.io/features/locations.html
+    install(Authentication) {
+        // "Implement API key auth (access_token) for parameter name 'api_key'."
+        apiKeyAuth("access_token") {
+            validate { apikeyCredential: ApiKeyCredential ->
+                when {
+                    apikeyCredential.value == "keyboardcat" -> ApiPrincipal(apikeyCredential)
+                    else -> null
+                }
+            }
+        }
+    }
+    install(Routing) {
+        ActionApi()
+        CredentialApi()
+        OrganizationApi()
+        PolicyApi()
+        ResourceTypeApi()
+        TokenApi()
+        UsersApi()
+    }
+
+
+    environment.monitor.subscribe(ApplicationStopping)
+    {
+        HTTP.client.close()
+    }
+}
