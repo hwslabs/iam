4d978e622ee8f4e0491bc24ecf0965cdd20a8546
Karthick Vinod <karthvin@hypto.in>
2022-02-08 17:10:13 +0530

    Integrate Jooq and move Fliway migration from code to gradle task

--- src/main/kotlin/com/hypto/iam/server/Application.kt
+++ src/main/kotlin/com/hypto/iam/server/Application.kt
@@ -1,90 +1,21 @@
 package com.hypto.iam.server
 
-import com.codahale.metrics.Slf4jReporter
-import com.typesafe.config.ConfigFactory
-import io.ktor.application.Application
-import io.ktor.application.ApplicationStopping
-import io.ktor.application.install
-import io.ktor.application.log
-import io.ktor.auth.*
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.config.HoconApplicationConfig
-import io.ktor.gson.GsonConverter
-import io.ktor.http.ContentType
-import io.ktor.locations.KtorExperimentalLocationsAPI
-import io.ktor.locations.Locations
-import io.ktor.routing.Routing
-import java.util.concurrent.TimeUnit
-import io.ktor.util.KtorExperimentalAPI
-import io.ktor.metrics.dropwizard.DropwizardMetrics
-import com.hypto.iam.server.apis.ActionApi
-import com.hypto.iam.server.apis.CredentialApi
 import com.hypto.iam.server.apis.OrganizationApi
-import com.hypto.iam.server.apis.PolicyApi
-import com.hypto.iam.server.apis.ResourceTypeApi
-import com.hypto.iam.server.apis.TokenApi
-import com.hypto.iam.server.apis.UsersApi
+import com.hypto.iam.server.db.models.tables.Organizations
 import com.hypto.iam.server.service.DatabaseFactory
-import com.typesafe.config.ConfigFactory.defaultApplication
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.slf4j.LoggerFactory
-
-//@KtorExperimentalAPI
-//internal val settings = HoconApplicationConfig(ConfigFactory.defaultApplication(HTTP::class.java.classLoader))
-//
-//object HTTP {
-//    val client = HttpClient(Apache)
-//}
-//
-//@KtorExperimentalAPI
-//@KtorExperimentalLocationsAPI
-//fun Application.main() {
-//    install(DefaultHeaders)
-//    install(DropwizardMetrics) {
-//        val reporter = Slf4jReporter.forRegistry(registry)
-//                .outputTo(log)
-//                .convertRatesTo(TimeUnit.SECONDS)
-//                .convertDurationsTo(TimeUnit.MILLISECONDS)
-//                .build()
-//        reporter.start(10, TimeUnit.SECONDS)
-//    }
-//    install(ContentNegotiation) {
-//        register(ContentType.Application.Json, GsonConverter())
-//    }
-//    install(AutoHeadResponse) // see http://ktor.io/features/autoheadresponse.html
-//    install(HSTS, ApplicationHstsConfiguration()) // see http://ktor.io/features/hsts.html
-//    install(Compression, ApplicationCompressionConfiguration()) // see http://ktor.io/features/compression.html
-//    install(Locations) // see http://ktor.io/features/locations.html
-//    install(Authentication) {
-////        basic {  }
-////        bearer {
-////            refreshTokensFun = { BearerTokens("valid", "refresh") }
-////            loadTokensFun = { BearerTokens("invalid", "refresh") }
-////        }
-//    }
-//
-//    DatabaseFactory.connectAndMigrate()
-//
-//    install(Routing) {
-//        ActionApi()
-//        CredentialApi()
-//        OrganizationApi()
-//        PolicyApi()
-//        ResourceTypeApi()
-//        TokenApi()
-//        UsersApi()
-//    }
-//
-//
-//    environment.monitor.subscribe(ApplicationStopping)
-//    {
-//        HTTP.client.close()
-//    }
-//}
+import org.jooq.Record
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+//import java.util.concurrent.Flow.Publisher
 
 @KtorExperimentalLocationsAPI
 fun Application.module() {
@@ -112,7 +43,9 @@ fun Application.module() {
     // TODO: Add support for Bearer token authentication
 //    install(Authentication) {}
 
-    DatabaseFactory.connectAndMigrate()
+//    val conn = DatabaseFactory.getConnection()
+//    val create = DSL.using(conn, SQLDialect.POSTGRES)
+//    val result: Result<Record> = create.select().from(Organizations()).fetch()
 
     install(Routing) {
 //        ActionApi()
@@ -128,7 +61,8 @@ fun Application.module() {
 @KtorExperimentalLocationsAPI
 fun main(args: Array<String>) {
     embeddedServer(
-        Netty, 8080, module = Application::module, watchPaths = listOf("classes", "resources")
+        Netty, 8081, module = Application::module, watchPaths = listOf("classes", "resources")
     ).start(wait = true)
 }
+
 
