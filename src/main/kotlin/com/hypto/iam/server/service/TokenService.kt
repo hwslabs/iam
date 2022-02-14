3524fc9176a0446c5e8537f38ba7c8aedaec8c54
Karthick Vinod <karthvin@hypto.in>
2022-02-14 21:23:45 +0530

    [In-progress] Adding JWT token generation logic

--- src/main/kotlin/com/hypto/iam/server/service/TokenService.kt
+++ src/main/kotlin/com/hypto/iam/server/service/TokenService.kt
@@ -4,26 +4,26 @@
 package com.hypto.iam.server.service
 
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import java.time.Instant
 import java.util.Date
-import sun.security.ec.ECPrivateKeyImpl
-import sun.security.util.DerValue
 
-class TokenService {
-    companion object {
-        const val ISSUER = "https://iam.hypto.com"
-        const val TOKEN_VALIDITY: Long = 3600 // in seconds
-        const val VERSION_CLAIM = "ver"
-        const val USER_CLAIM = "usr"
-        const val ORGANIZATION_CLAIM = "org"
-        const val ENTITLEMENTS_CLAIM = "entitlements"
-//        var publicKeyString: String = ""
-        var privateKeyString: String = ""
-//
-//        var keyPair: KeyPair = KeyPair(ECPublicKeyImpl.parse(DerValue(publicKeyString)), ECPrivateKeyImpl.parseKey(DerValue(privateKeyString)))
-        val privateKey = ECPrivateKeyImpl.parseKey(DerValue(privateKeyString))
-    }
+object TokenService {
+    private const val ISSUER = "https://iam.hypto.com"
+    private const val TOKEN_VALIDITY: Long = 3600 // in seconds
+    private const val VERSION_CLAIM = "ver"
+    private const val USER_CLAIM = "usr"
+    private const val ORGANIZATION_CLAIM = "org"
+    private const val ENTITLEMENTS_CLAIM = "entitlements"
+
+    var privateKey: PrivateKey = AssymetricKey("private_key.der", "public_key.der").privateKey
 
     fun generateJwtToken(userId: String, orgId: String, entitlements: String): String {
         // 1. Validate user
@@ -41,14 +41,50 @@ class TokenService {
             .claim(VERSION_CLAIM, "1.0")
             .claim(USER_CLAIM, userId.toString()) // UserId
             .claim(ORGANIZATION_CLAIM, orgId) // OrganizationId
-//            .claim(ENTITLEMENTS_CLAIM, fetchEntitlements(userId))
-            .signWith(privateKey, SignatureAlgorithm.ES256)
+            .claim(ENTITLEMENTS_CLAIM, "fetchEntitlements(userId)")
+            .signWith(privateKey/*, SignatureAlgorithm.ES256*/)
             .compact()
-        return ""
+        return builder
     }
 
 //    fun fetchEntitlements(userId: String): String {
 //        val policies = UserPoliciesRepo.fetchByPrincipalHrn(userId)
 //        PoliciesRepo.g
 //    }
+}
+
+/**
+Generate EC key pair using the below commands:
+- Create a ES256 private key pem:
+`openssl ecparam -name prime256v1 -genkey -noout -out private_key.pem`
+- Generate corresponding  public key pem:
+`openssl ec -in private_key.pem -pubout -out public_key.pem`
+- Generate private_key.der corresponding to private_key.pem
+`openssl pkcs8 -topk8 -inform PEM -outform DER -in  private_key.pem -out  private_key.der -nocrypt`
+- Generate public_key.der corresponding to private_key.pem
+`openssl ec -in private_key.pem -pubout -outform DER -out public_key.der`
+* */
+class AssymetricKey(private val privateKeyByteArray: ByteArray, private val publicKeyByteArray: ByteArray) {
+    constructor(privateKeyFile: String, publicKeyFile: String) :
+        this(readResourceFileAsBytes(privateKeyFile), readResourceFileAsBytes(publicKeyFile))
+
+    private val keyFactory: KeyFactory = KeyFactory.getInstance("EC")
+
+    val publicKey: PublicKey
+        get() {
+            val keySpec = X509EncodedKeySpec(publicKeyByteArray)
+            return keyFactory.generatePublic(keySpec)
+        }
+    val privateKey: PrivateKey
+        get() {
+            val keySpec = PKCS8EncodedKeySpec(privateKeyByteArray)
+            return keyFactory.generatePrivate(keySpec)
+        }
+
+    companion object {
+        fun readResourceFileAsBytes(name: String): ByteArray {
+            val fileUri: URI = this::class.java.classLoader.getResource(name).toURI()
+            return Files.readAllBytes(Paths.get(fileUri))
+        }
+    }
 }
--- src/test/kotlin/com/hypto/iam/server/ConfigurationTest.kt
+++ src/test/kotlin/com/hypto/iam/server/ConfigurationTest.kt
@@ -1,5 +1,6 @@
 package com.hypto.iam.server
 
+import com.hypto.iam.server.service.TokenService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -7,5 +8,6 @@ internal class ConfigurationTest {
     @Test
     fun `Sample Test`() {
         Assertions.assertEquals(1, 1)
+        println(TokenService.generateJwtToken("u", "o", "{some json indicating entitlements}"))
     }
 }
