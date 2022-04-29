59c387ba65444887984cb75533666198cd1f317f
hari-hypto <hari@hypto.in>
2022-04-29 18:16:13 +0530

    Hoplite backward incompatibility issue fix
    - Added env property source in hoplite config
    - Updated default value for logback log destination variables.
    
    Test: docker-compose up is up and running.

--- src/main/kotlin/com/hypto/iam/server/configs/AppConfig.kt
+++ src/main/kotlin/com/hypto/iam/server/configs/AppConfig.kt
@@ -1,6 +1,7 @@
 package com.hypto.iam.server.configs
 
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.sources.EnvironmentVariablesPropertySource
 
 data class AppConfig(val app: App, val server: Server, val database: Database, val newrelic: Newrelic, val aws: Aws) {
     /**
@@ -94,6 +95,10 @@ data class AppConfig(val app: App, val server: Server, val database: Database, v
     data class Aws(val region: String, val accessKey: String, val secretKey: String)
 
     companion object {
-        val configuration: AppConfig = ConfigLoader().loadConfigOrThrow("/default_config.json")
+        private val configLoader = ConfigLoader.builder().addPropertySource(EnvironmentVariablesPropertySource(
+            useUnderscoresAsSeparator = true,
+            allowUppercaseNames = true
+        )).build()
+        val configuration: AppConfig = configLoader.loadConfigOrThrow("/default_config.json")
     }
 }
