cff92bcc61f22009802edd60680fc64cd6c0650f
Anto Beslie <antobeslie@hypto.in>
2022-12-15 16:37:54 +0530

    Index columns changed

--- src/main/resources/db/migration/V12__add_deleted_to_resources_actions.sql
+++ src/main/resources/db/migration/V12__add_deleted_to_resources_actions.sql
@@ -1,7 +1,9 @@
 ALTER TABLE resources
     ADD COLUMN deleted BOOLEAN DEFAULT false;
-CREATE INDEX resources_deleted ON resources(deleted);
+DROP INDEX IF EXISTS resources_idx_org_id;
+CREATE INDEX ON resources (organization_id) WHERE deleted IS FALSE;
 
 ALTER TABLE actions
     ADD COLUMN deleted BOOLEAN DEFAULT false;
-CREATE INDEX actions_deleted ON actions(deleted);
+DROP INDEX IF EXISTS actions_idx_org_id_resource_hrn;
+CREATE INDEX ON actions (organization_id, resource_hrn) WHERE deleted IS FALSE;
