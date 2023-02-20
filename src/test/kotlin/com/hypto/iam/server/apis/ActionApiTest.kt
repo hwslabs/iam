d0a122ab2216c0e11648b7eb91a6cf6bccfe5ae7
Venkat Narayan TR <venkatttwenty1@gmail.com>
2023-02-20 12:58:30 +0530

    Updated ActionApiTest with new syntax

--- src/test/kotlin/com/hypto/iam/server/apis/ActionApiTest.kt
+++ src/test/kotlin/com/hypto/iam/server/apis/ActionApiTest.kt
@@ -2,24 +2,30 @@ package com.hypto.iam.server.apis
 
 import com.google.gson.Gson
 import com.hypto.iam.server.Constants
-import com.hypto.iam.server.handleRequest
 import com.hypto.iam.server.helpers.AbstractContainerBaseTest
-import com.hypto.iam.server.helpers.DataSetupHelper
+import com.hypto.iam.server.helpers.DataSetupHelperV2.createAction
+import com.hypto.iam.server.helpers.DataSetupHelperV2.createOrganization
+import com.hypto.iam.server.helpers.DataSetupHelperV2.createResource
+import com.hypto.iam.server.helpers.DataSetupHelperV2.deleteOrganization
 import com.hypto.iam.server.models.Action
 import com.hypto.iam.server.models.ActionPaginatedResponse
 import com.hypto.iam.server.models.BaseSuccessResponse
 import com.hypto.iam.server.models.CreateActionRequest
 import com.hypto.iam.server.models.UpdateActionRequest
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.http.withCharset
-import io.ktor.server.application.Application
-import io.ktor.server.testing.contentType
-import io.ktor.server.testing.handleRequest
-import io.ktor.server.testing.setBody
-import io.ktor.server.testing.withTestApplication
+import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.koin.test.inject
@@ -28,211 +34,199 @@ class ActionApiTest : AbstractContainerBaseTest() {
     private val gson: Gson by inject()
 
     @Test
-    fun `create action success case`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse, _) = DataSetupHelper.createOrganization(this)
+    fun `create action success case1`() {
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse, _) = createOrganization()
+
             val organization = organizationResponse.organization!!
             val rootUserToken = organizationResponse.rootUserToken!!
-            val resource = DataSetupHelper.createResource(organization.id, rootUserToken, this)
-
-            with(
-                handleRequest(HttpMethod.Post, "/organizations/${organization.id}/resources/${resource.name}/actions") {
-                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                    setBody(gson.toJson(CreateActionRequest(name = "test-action")))
-                }
-            ) {
-                val responseBody = gson.fromJson(response.content, Action::class.java)
-                assertEquals(HttpStatusCode.Created, response.status())
-                assertEquals(
-                    ContentType.Application.Json.withCharset(Charsets.UTF_8),
-                    response.contentType()
-                )
-                assertEquals(
-                    organization.id,
-                    response.headers[Constants.X_ORGANIZATION_HEADER]
-                )
-
-                assertEquals(organization.id, responseBody.organizationId)
-                assertEquals("", responseBody.description)
+            val resource = createResource(organization.id, rootUserToken)
+            val response = client.post("/organizations/${organization.id}/resources/${resource.name}/actions") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
+                setBody(gson.toJson(CreateActionRequest(name = "test-action")))
             }
+            val responseBody = gson.fromJson(response.bodyAsText(), Action::class.java)
+            assertEquals(HttpStatusCode.Created, response.status)
+            assertEquals(
+                ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                response.contentType()
+            )
+            assertEquals(
+                organization.id,
+                response.headers[Constants.X_ORGANIZATION_HEADER]
+            )
 
-            DataSetupHelper.deleteOrganization(organization.id, this)
+            assertEquals(organization.id, responseBody.organizationId)
+            assertEquals("", responseBody.description)
+
+            deleteOrganization(organization.id)
         }
     }
 
     @Test
     fun `get action`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse, _) = DataSetupHelper.createOrganization(this)
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse, _) = createOrganization()
             val organization = organizationResponse.organization!!
             val rootUserToken = organizationResponse.rootUserToken!!
 
-            val (action, resource) = DataSetupHelper.createAction(organization.id, null, rootUserToken, this)
+            val (action, resource) = createAction(organization.id, null, rootUserToken)
 
-            with(
-                handleRequest(
-                    HttpMethod.Get,
-                    "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                }
+            val response = client.get(
+                "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
             ) {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    ContentType.Application.Json.withCharset(Charsets.UTF_8),
-                    response.contentType()
-                )
-
-                val responseBody = gson.fromJson(response.content, Action::class.java)
-                assertEquals(action.name, responseBody.name)
-                assertEquals(organization.id, responseBody.organizationId)
-                assertEquals(resource.name, responseBody.resourceName)
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
             }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                response.contentType()
+            )
 
-            DataSetupHelper.deleteOrganization(organization.id, this)
+            val responseBody = gson.fromJson(response.bodyAsText(), Action::class.java)
+            assertEquals(action.name, responseBody.name)
+            assertEquals(organization.id, responseBody.organizationId)
+            assertEquals(resource.name, responseBody.resourceName)
+
+            deleteOrganization(organization.id)
         }
     }
 
     @Test
     fun `get action failure for unauthorized user access`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse1, _) = DataSetupHelper.createOrganization(this)
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse1, _) = createOrganization()
             val organization1 = organizationResponse1.organization!!
             val rootUserToken1 = organizationResponse1.rootUserToken!!
 
-            val (organizationResponse2, _) = DataSetupHelper.createOrganization(this)
+            val (organizationResponse2, _) = createOrganization()
             val organization2 = organizationResponse2.organization!!
             val rootUserToken2 = organizationResponse2.rootUserToken!!
 
-            val (action, resource) = DataSetupHelper.createAction(organization1.id, null, rootUserToken1, this)
+            val (action, resource) = createAction(organization1.id, null, rootUserToken1)
 
-            with(
-                handleRequest(
-                    HttpMethod.Get,
-                    "/organizations/${organization1.id}/resources/${resource.name}/actions/${action.name}"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken2")
-                }
+            val response = client.get(
+                "/organizations/${organization1.id}/resources/${resource.name}/actions/${action.name}"
             ) {
-                assertEquals(HttpStatusCode.Forbidden, response.status())
-                assertEquals(
-                    ContentType.Application.Json.withCharset(Charsets.UTF_8),
-                    response.contentType()
-                )
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken2")
             }
+            assertEquals(HttpStatusCode.Forbidden, response.status)
+            assertEquals(
+                ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                response.contentType()
+            )
 
-            DataSetupHelper.deleteOrganization(organization1.id, this)
-            DataSetupHelper.deleteOrganization(organization2.id, this)
+            deleteOrganization(organization1.id)
+            deleteOrganization(organization2.id)
         }
     }
 
     @Test
     fun `delete action`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse, _) = DataSetupHelper.createOrganization(this)
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse, _) = createOrganization()
             val organization = organizationResponse.organization!!
             val rootUserToken = organizationResponse.rootUserToken!!
 
-            val (action, resource) = DataSetupHelper.createAction(organization.id, null, rootUserToken, this)
-
-            with(
-                handleRequest(
-                    HttpMethod.Delete,
-                    "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                }
+            val (action, resource) = createAction(organization.id, null, rootUserToken)
+            var response = client.delete(
+                "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
             ) {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(ContentType.Application.Json.withCharset(Charsets.UTF_8), response.contentType())
-                val responseBody = gson.fromJson(response.content, BaseSuccessResponse::class.java)
-                assertEquals(true, responseBody.success)
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
             }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(ContentType.Application.Json.withCharset(Charsets.UTF_8), response.contentType())
+            val responseBody = gson.fromJson(response.bodyAsText(), BaseSuccessResponse::class.java)
+            assertEquals(true, responseBody.success)
 
-            // Check that the action is not available
-            with(
-                handleRequest(
-                    HttpMethod.Get,
-                    "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                }
+            // Check that the action is not availabl
+            response = client.get(
+                "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
             ) {
-                assertEquals(HttpStatusCode.NotFound, response.status())
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
             }
+            assertEquals(HttpStatusCode.NotFound, response.status)
 
-            DataSetupHelper.deleteOrganization(organization.id, this)
+            deleteOrganization(organization.id)
         }
     }
 
     @Test
     fun `list actions for a resource`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse, _) = DataSetupHelper.createOrganization(this)
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse, _) = createOrganization()
             val organization = organizationResponse.organization!!
             val rootUserToken = organizationResponse.rootUserToken!!
 
-            val (action1, resource) = DataSetupHelper.createAction(organization.id, null, rootUserToken, this)
-            val (action2, _) = DataSetupHelper.createAction(organization.id, resource, rootUserToken, this)
-
-            with(
-                handleRequest(
-                    HttpMethod.Get,
-                    "/organizations/${organization.id}/resources/${resource.name}/actions"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                }
+            val (action1, resource) = createAction(organization.id, null, rootUserToken,)
+            val (action2, _) = createAction(organization.id, resource, rootUserToken)
+            val response = client.get(
+                "/organizations/${organization.id}/resources/${resource.name}/actions"
             ) {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    ContentType.Application.Json.withCharset(Charsets.UTF_8),
-                    response.contentType()
-                )
-
-                val responseBody = gson.fromJson(response.content, ActionPaginatedResponse::class.java)
-                assertEquals(2, responseBody.data!!.size)
-                assert(responseBody.data!!.contains(action1))
-                assert(responseBody.data!!.contains(action2))
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
             }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                response.contentType()
+            )
 
-            DataSetupHelper.deleteOrganization(organization.id, this)
+            val responseBody = gson.fromJson(response.bodyAsText(), ActionPaginatedResponse::class.java)
+            assertEquals(2, responseBody.data!!.size)
+            assert(responseBody.data!!.contains(action1))
+            assert(responseBody.data!!.contains(action2))
+
+            deleteOrganization(organization.id)
         }
     }
 
     @Test
     fun `update action`() {
-        withTestApplication(Application::handleRequest) {
-            val (organizationResponse, _) = DataSetupHelper.createOrganization(this)
+        testApplication {
+            environment {
+                config = ApplicationConfig("application-custom.conf")
+            }
+            val (organizationResponse, _) = createOrganization()
             val organization = organizationResponse.organization!!
             val rootUserToken = organizationResponse.rootUserToken!!
 
-            val (action, resource) = DataSetupHelper.createAction(organization.id, null, rootUserToken, this)
+            val (action, resource) = createAction(organization.id, null, rootUserToken)
             val newDescription = "new description"
-
-            with(
-                handleRequest(
-                    HttpMethod.Patch,
-                    "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
-                ) {
-                    addHeader(HttpHeaders.Authorization, "Bearer $rootUserToken")
-                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                    setBody(gson.toJson(UpdateActionRequest(description = "new description")))
-                }
+            val response = client.patch(
+                "/organizations/${organization.id}/resources/${resource.name}/actions/${action.name}"
             ) {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    ContentType.Application.Json.withCharset(Charsets.UTF_8),
-                    response.contentType()
-                )
-
-                val responseBody = gson.fromJson(response.content, Action::class.java)
-                assertEquals(action.name, responseBody.name)
-                assertEquals(resource.name, responseBody.resourceName)
-                assertEquals(newDescription, responseBody.description)
+                header(HttpHeaders.Authorization, "Bearer $rootUserToken")
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(gson.toJson(UpdateActionRequest(description = "new description")))
             }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                ContentType.Application.Json.withCharset(Charsets.UTF_8),
+                response.contentType()
+            )
 
-            DataSetupHelper.deleteOrganization(organization.id, this)
+            val responseBody = gson.fromJson(response.bodyAsText(), Action::class.java)
+            assertEquals(action.name, responseBody.name)
+            assertEquals(resource.name, responseBody.resourceName)
+            assertEquals(newDescription, responseBody.description)
+
+            deleteOrganization(organization.id)
         }
     }
 }
---
+++ src/test/kotlin/com/hypto/iam/server/helpers/DataSetupHelperV2.kt
@@ -0,0 +1,237 @@
+package com.hypto.iam.server.helpers
+
+import com.google.gson.Gson
+import com.hypto.iam.server.Constants
+import com.hypto.iam.server.configs.AppConfig
+import com.hypto.iam.server.db.Tables.ACTIONS
+import com.hypto.iam.server.db.Tables.PRINCIPAL_POLICIES
+import com.hypto.iam.server.db.Tables.RESOURCES
+import com.hypto.iam.server.db.repositories.ActionRepo
+import com.hypto.iam.server.db.repositories.OrganizationRepo
+import com.hypto.iam.server.db.repositories.PoliciesRepo
+import com.hypto.iam.server.db.repositories.PrincipalPoliciesRepo
+import com.hypto.iam.server.db.repositories.ResourceRepo
+import com.hypto.iam.server.models.Action
+import com.hypto.iam.server.models.CreateActionRequest
+import com.hypto.iam.server.models.CreateCredentialRequest
+import com.hypto.iam.server.models.CreateOrganizationRequest
+import com.hypto.iam.server.models.CreateOrganizationResponse
+import com.hypto.iam.server.models.CreateResourceRequest
+import com.hypto.iam.server.models.Credential
+import com.hypto.iam.server.models.Resource
+import com.hypto.iam.server.models.RootUser
+import com.hypto.iam.server.utils.ActionHrn
+import com.hypto.iam.server.utils.IdGenerator
+import com.hypto.iam.server.utils.ResourceHrn
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.server.testing.ApplicationTestBuilder
+import kotlinx.coroutines.runBlocking
+import org.koin.test.inject
+import org.koin.test.junit5.AutoCloseKoinTest
+
+object DataSetupHelperV2 : AutoCloseKoinTest() {
+    private val gson: Gson by inject()
+    private val appConfig: AppConfig by inject()
+    private val rootToken = Constants.SECRET_PREFIX + appConfig.app.secretKey
+
+    private val organizationRepo: OrganizationRepo by inject()
+    private val policyRepo: PoliciesRepo by inject()
+    private val principalPoliciesRepo: PrincipalPoliciesRepo by inject()
+    private val resourceRepo: ResourceRepo by inject()
+    private val actionRepo: ActionRepo by inject()
+
+    suspend fun ApplicationTestBuilder.createOrganization(
+        preferredUsername: String = "user" + IdGenerator.randomId()
+    ): Pair<CreateOrganizationResponse, RootUser> {
+        // Create organization
+        val orgName = "test-org" + IdGenerator.randomId()
+        val name = "lorem ipsum"
+        val testEmail = "test-email" + IdGenerator.randomId() + "@iam.in"
+        val testPhone = "+919626012778"
+        val testPassword = "testPassword@Hash1"
+
+        val rootUser = RootUser(
+            preferredUsername = preferredUsername,
+            name = name,
+            password = testPassword,
+            email = testEmail,
+            phone = testPhone
+        )
+
+        val createOrganizationCall = client.post("/organizations") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            header("X-Api-Key", rootToken)
+            setBody(
+                gson.toJson(
+                    CreateOrganizationRequest(
+                        orgName,
+                        rootUser
+                    )
+                )
+            )
+        }
+
+        val createdOrganizationResponse = gson
+            .fromJson(createOrganizationCall.bodyAsText(), CreateOrganizationResponse::class.java)
+
+        return Pair(createdOrganizationResponse, rootUser.copy(email = rootUser.email.lowercase()))
+    }
+
+    suspend fun ApplicationTestBuilder.createCredential(
+        orgId: String,
+        userName: String,
+        jwtToken: String,
+    ): Credential {
+        val createCredentialCall = client.post(
+            "/organizations/$orgId/users/$userName/credentials"
+        ) {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            header(HttpHeaders.Authorization, "Bearer $jwtToken")
+            setBody(gson.toJson(CreateCredentialRequest()))
+        }
+
+        return gson
+            .fromJson(createCredentialCall.bodyAsText(), Credential::class.java)
+    }
+
+//    suspend fun ApplicationTestBuilder.createUser(
+//        orgId: String,
+//        bearerToken: String,
+//        createUserRequest: CreateUserRequest,
+//    ): Pair<User, Credential> {
+//        val createUserCall = client.post("/organizations/$orgId/users") {
+//            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+//            header(HttpHeaders.Authorization, "Bearer $bearerToken")
+//            setBody(gson.toJson(createUserRequest))
+//        }
+//        val createdUser = gson
+//            .fromJson(createUserCall.bodyAsText(), CreateUserResponse::class.java)
+//        val credential =
+//            createCredential(orgId, createdUser.user.username, bearerToken)
+//
+//        return Pair(createdUser.user, credential)
+//    }
+//
+//    suspend fun ApplicationTestBuilder.createAndAttachPolicy(
+//        orgId: String,
+//        username: String?,
+//        bearerToken: String,
+//        policyName: String,
+//        accountId: String?,
+//        resourceName: String,
+//        actionName: String,
+//        resourceInstance: String,
+//        effect: PolicyStatement.Effect = PolicyStatement.Effect.allow
+//    ): Policy {
+//        val (resourceHrn, actionHrn) = createResourceActionHrn(
+//            orgId,
+//            accountId,
+//            resourceName,
+//            actionName,
+//            resourceInstance
+//        )
+//        val policyStatements = listOf(PolicyStatement(resourceHrn, actionHrn, effect))
+//        val requestBody = CreatePolicyRequest(policyName, policyStatements)
+//
+//        val createPolicyCall = client.post(
+//            "/organizations/$orgId/policies"
+//        ) {
+//            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+//            header(HttpHeaders.Authorization, "Bearer $bearerToken")
+//            setBody(gson.toJson(requestBody))
+//        }
+//
+//        val policy = gson.fromJson(createPolicyCall.bodyAsText(), Policy::class.java)
+//
+//        if (username != null) {
+//            client.patch(
+//                "/organizations/$orgId/users/" +
+//                    "$username/attach_policies"
+//            ) {
+//                val createAssociationRequest = PolicyAssociationRequest(listOf(policy.hrn))
+//                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+//                header(
+//                    HttpHeaders.Authorization,
+//                    "Bearer $bearerToken"
+//                )
+//                setBody(gson.toJson(createAssociationRequest))
+//            }
+//        }
+//        return policy
+//    }
+
+    suspend fun ApplicationTestBuilder.createResource(
+        orgId: String,
+        jwtToken: String,
+        resourceName: String? = null
+    ): Resource {
+        val name = resourceName ?: ("test-resource" + IdGenerator.randomId())
+
+        val createResourceCall = client.post("/organizations/$orgId/resources") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            header(HttpHeaders.Authorization, "Bearer $jwtToken")
+            setBody(gson.toJson(CreateResourceRequest(name = name)))
+        }
+
+        return gson
+            .fromJson(createResourceCall.bodyAsText(), Resource::class.java)
+    }
+
+    suspend fun ApplicationTestBuilder.createAction(
+        orgId: String,
+        resource: Resource? = null,
+        jwtToken: String,
+    ): Pair<Action, Resource> {
+        val createdResource = resource ?: createResource(orgId, jwtToken)
+        val actionName = "test-action" + IdGenerator.randomId()
+
+        val createActionCall =
+            client.post("/organizations/$orgId/resources/${createdResource.name}/actions") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                header(HttpHeaders.Authorization, "Bearer $jwtToken")
+                setBody(gson.toJson(CreateActionRequest(name = actionName)))
+            }
+
+        val createdAction = gson
+            .fromJson(createActionCall.bodyAsText(), Action::class.java)
+
+        return Pair(createdAction, createdResource)
+    }
+
+    fun createResourceActionHrn(
+        orgId: String,
+        accountId: String?,
+        resourceName: String,
+        actionName: String,
+        resourceInstance: String? = null
+    ): Pair<String, String> {
+        val resourceHrn = ResourceHrn(orgId, accountId, resourceName, resourceInstance).toString()
+        val actionHrn = ActionHrn(orgId, accountId, resourceName, actionName).toString()
+        return Pair(resourceHrn, actionHrn)
+    }
+
+    // This function is used for cleaning up all the data created during the test for the organization
+    fun deleteOrganization(orgId: String) {
+        runBlocking {
+            // TODO: Optimize the queries to do it one query (CASCADE DELETE)
+            policyRepo.fetchByOrganizationId(orgId).forEach { policies ->
+                principalPoliciesRepo.fetch(PRINCIPAL_POLICIES.POLICY_HRN, policies.hrn).forEach {
+                    principalPoliciesRepo.delete(it)
+                }
+                policyRepo.delete(policies)
+            }
+            resourceRepo.fetch(RESOURCES.ORGANIZATION_ID, orgId).forEach { resource ->
+                actionRepo.fetch(ACTIONS.RESOURCE_HRN, resource.hrn).forEach {
+                    actionRepo.delete(it)
+                }
+                resourceRepo.delete(resource)
+            }
+            organizationRepo.deleteById(orgId)
+        }
+    }
+}
