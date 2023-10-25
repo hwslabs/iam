86485966430a323c60e64c8c83282f59f2d576e0
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2023-10-25 14:57:39 +0530

    :bug: fix: rollback if email already exists

---
+++ src/main/kotlin/com/hypto/iam/server/exceptions/DbExceptionHandler.kt
@@ -0,0 +1,62 @@
+package com.hypto.iam.server.exceptions
+
+import io.ktor.server.plugins.BadRequestException
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+import org.jooq.exception.DataAccessException
+
+object DbExceptionHandler {
+    data class DbExceptionMap(
+        val errorMessage: String,
+        val constraintRegex: Regex,
+        val appExceptions: Map<String, Pair<KClass<out Exception>, String>>
+    )
+
+    private val customExceptions = setOf(
+        BadRequestException::class,
+        EntityNotFoundException::class,
+        EntityAlreadyExistsException::class
+    )
+    private val duplicateConstraintRegex = "\"(.*)?\"".toRegex()
+    private val foreignKeyConstraintRegex = "foreign key constraint \"(.*)?\"".toRegex()
+
+    private val duplicateExceptions = mapOf<String, Pair<KClass<out Exception>, String>>()
+
+    private val foreignKeyExceptions = mapOf<String, Pair<KClass<out Exception>, String>>()
+
+    private val dbExceptionMap = listOf(
+        DbExceptionMap(
+            "duplicate key value violates unique constraint",
+            duplicateConstraintRegex,
+            duplicateExceptions
+        ),
+        DbExceptionMap(
+            "violates foreign key constraint",
+            foreignKeyConstraintRegex,
+            foreignKeyExceptions
+        )
+    )
+
+    fun mapToApplicationException(e: DataAccessException): Exception {
+        var finalException: Exception? = null
+        val causeMessage = e.cause?.message ?: e.message
+
+        e.cause?.let {
+            if (customExceptions.contains(it::class)) {
+                return it as Exception
+            }
+        }
+
+        dbExceptionMap.forEach {
+            if (causeMessage?.contains(it.errorMessage) == true) {
+                val constraintKey = it.constraintRegex.find(causeMessage)?.groups?.get(1)?.value
+                val exceptionPair = it.appExceptions[constraintKey]
+                if (exceptionPair != null) {
+                    finalException = exceptionPair.first.primaryConstructor?.call(exceptionPair.second, e)
+                }
+            }
+        }
+
+        return finalException ?: InternalException("Unknown error occurred", e)
+    }
+}
