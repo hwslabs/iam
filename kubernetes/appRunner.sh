78ed31c450fbc45d795ff679d8cd0ed4d8b877ad
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2024-04-03 18:33:22 +0530

    :wrench: chore: add migration to appRunner shell

---
+++ kubernetes/appRunner.sh
@@ -0,0 +1,2 @@
+java -cp /opt/app/app.jar com.hypto.iam.server.MigrationHandler
+java -Xlog:gc*=debug:file=/opt/app_logs/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/app_logs/heap-dumps -XX:MaxMetaspaceSize=398928K  -XX:InitialRAMPercentage=70.0 -XX:MaxRAMPercentage=70.0 -jar /opt/app/app.jar
