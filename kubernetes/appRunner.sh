cb81de1ac984953afb24bfabc7a2c1e00ce5e7a4
Aravind Kannan Rathinasabapathi <55327432+Aravind-Kannan@users.noreply.github.com>
2024-05-18 15:26:29 +0530

    :wrench: chore: clean up kubernetes folder (#205)

--- kubernetes/appRunner.sh
+++ kubernetes/appRunner.sh
@@ -1,2 +1,5 @@
+# Option e: Exit immediately if a command exits with a non-zero status
+set -e
+
 java -cp /opt/app/app.jar com.hypto.iam.server.MigrationHandler
 java -Xlog:gc*=debug:file=/opt/app_logs/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/app_logs/heap-dumps -XX:MaxMetaspaceSize=398928K  -XX:InitialRAMPercentage=70.0 -XX:MaxRAMPercentage=70.0 -jar /opt/app/app.jar
